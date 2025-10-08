be41ab48ba5d7347dadc5ea6a29899405d0c1c00
grazielac <cpgraziela@gmail.com>
2025-10-08 21:19:25 +0100

    Actual url showing, not image

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,21 +3,28 @@ import { useEffect, useState } from "react";
 export default function Gallery() {
   const [photos, setPhotos] = useState([]);
 
-  useEffect(() => { {/* you only want to fecth data once, when the component first loads */}
+  useEffect(() => {
+    {
+      /* you only want to fecth data once, when the component first loads */
+    }
     async function fetchData() {
       const response = await fetch("https://week-6-api.vercel.app/api/images");
       const data = await response.json();
       setPhotos(data);
     }
     fetchData();
-  }, []); {/* dependency array // runs the effect only once after the component's first render*/}
+  }, []);
+  {
+    /* dependency array // runs the effect only once after the component's first render*/
+  }
 
+  console.log(photos);
   return (
     <div>
       <h1>Photos</h1>
       <ul>
         {photos.map((photo) => (
-          <li key={photo.id}>{photo.title}</li>
+          <li key={photo.id}>{photo.url}</li>
         ))}
       </ul>
     </div>
