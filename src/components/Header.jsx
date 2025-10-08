d3aae4ffad114e92ab25688e6448dde6f19b3a41
grazielac <cpgraziela@gmail.com>
2025-10-08 18:25:21 +0100

    Change site logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-import Logo from "../assets/logo.png";
+import Vd from "../assets/vd.png";
 
 export default function Header() {
   return (
-    <div className="bg-amber-400 p-4 flex items-center">
-      <p>Visual Diary</p>
-      <img src={Logo} alt="Site Logo" />
-      <h1 className="bg-indigo-300">HELLO WORLD</h1>
-    </div>
+    <header className="bg-amber-400 flex items-center">
+      <img src={Vd} alt="Site Logo" className="h-10 m-4" />
+    </header>
   );
 }
