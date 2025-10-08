eb549b294e9b100064ed79001182f7246ab635dd
grazielac <cpgraziela@gmail.com>
2025-10-08 21:52:32 +0100

    Add grid and responsive columns

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,27 +4,23 @@ export default function Gallery() {
   const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
-    {
-      /* you only want to fecth data once, when the component first loads */
-    }
+    /* you only want to fecth data once, when the component first loads */
     async function fetchData() {
       const response = await fetch("https://week-6-api.vercel.app/api/images");
       const data = await response.json();
       setPhotos(data);
     }
     fetchData();
-  }, []);
-  {
-    /* dependency array // runs the effect only once after the component's first render*/
-  }
+  }, []); /* dependency array // runs the effect only once after the component's first render*/
 
   console.log(photos);
   return (
-    <div>
-      <h1>Photos</h1>
-      <ul>
+    <div className="m-4">
+      <h1 className="mb-15 underline text-center">Photos</h1>
+      <ul className="grid grid-cols-4 md:grid-cols-3 gap-4 grid-rows-">
         {photos.map((photo) => (
-          <img key={photo.id} src={photo.url}></img>
+          /* for each photo, render an img with its src and key to id*/
+          <img className="object-cover w-full border rounded-xl" key={photo.id} src={photo.url}></img>
         ))}
       </ul>
     </div>
