79ebe0275b904c957ac233e617fe01cacac6906b
grazielac <cpgraziela@gmail.com>
2025-10-08 19:57:18 +0100

    Add hover effect on home menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,9 +13,9 @@ export default function Header() {
       {/* left: logo + home feed */}
       <div className="flex justify-between items-center ml-10">
         <img src={Vd} alt="Site Logo" className="h-10 m-4 mr-8 cursor-pointer" />
-        <div className="flex justify-between items-center cursor-pointer">
-          <a href="#" className="text-sm">Home Feed</a>
-          <SlMenu className="h-7 m-4 text-black-50" />
+        <div className="flex justify-between items-center cursor-pointer hover:bg-amber-300 hover:rounded-xl">
+          <a href="#" className="text-sm pl-4 mt-0">Home Feed</a>
+          <SlMenu className="h-7 m-4 mt-0 pt-4 text-black-50" />
         </div>
       </div>
       {/* right: profile + search + etc */}
