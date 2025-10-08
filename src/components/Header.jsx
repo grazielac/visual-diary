25663f27b2888514032d355e9e5c198b5ae23740
grazielac <cpgraziela@gmail.com>
2025-10-08 19:33:55 +0100

    Add gradient effect bg header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,25 @@ import { SlBubble } from "react-icons/sl";
 import { SlBell } from "react-icons/sl";
 import { SlMagnifier } from "react-icons/sl";
 import { SlUser } from "react-icons/sl";
+import { SlArrowDown } from "react-icons/sl";
 
 export default function Header() {
   return (
-    <header className="bg-amber-400 flex justify-between items-center">
+    <header className="bg-gradient-to-t from-white to-yellow-100 flex justify-between items-center">
 
       {/* left: logo + home feed */}
-      <div className="flex justify-between items-center">
-        <img src={Vd} alt="Site Logo" className="h-10 m-4" />
+      <div className="flex justify-between items-center ml-10">
+        <img src={Vd} alt="Site Logo" className="h-10 m-4 mr-8" />
         <a href="#" className="hover:underline mr-4 text-sm">Home Feed</a>
       </div>
       {/* right: profile + search + etc */}
-      <div className="flex">
+      {/* source: react-icons.github.io */}
+      <div className="flex mr-10">
         <SlMagnifier href="#" className="h-7 m-4 text-black-50" />
         <SlBell href="#" className="h-7 m-4 text-black-50" />
         <SlBubble href="#" className="h-7 m-4 text-black-50" />
         <SlUser href="#" className="h-7 m-4 text-black-50" />
-
+        <SlArrowDown href="#" className="h-7 m-4 text-black-50" />
       </div>
     </header>
   );
