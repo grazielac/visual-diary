b0b80d86a032b46126d33e5535ba7ba2b04f75c5
grazielac <cpgraziela@gmail.com>
2025-10-08 19:42:40 +0100

    Add cursor pointer on icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { SlBell } from "react-icons/sl";
 import { SlMagnifier } from "react-icons/sl";
 import { SlUser } from "react-icons/sl";
 import { SlArrowDown } from "react-icons/sl";
+import { SlMenu } from "react-icons/sl";
 
 export default function Header() {
   return (
@@ -11,17 +12,20 @@ export default function Header() {
 
       {/* left: logo + home feed */}
       <div className="flex justify-between items-center ml-10">
-        <img src={Vd} alt="Site Logo" className="h-10 m-4 mr-8" />
-        <a href="#" className="hover:underline mr-4 text-sm">Home Feed</a>
+        <img src={Vd} alt="Site Logo" className="h-10 m-4 mr-8 cursor-pointer" />
+        <div className="flex justify-between items-center cursor-pointer">
+          <a href="#" className="text-sm">Home Feed</a>
+          <SlMenu className="h-7 m-4 text-black-50" />
+        </div>
       </div>
       {/* right: profile + search + etc */}
       {/* source: react-icons.github.io */}
       <div className="flex mr-10">
-        <SlMagnifier href="#" className="h-7 m-4 text-black-50" />
-        <SlBell href="#" className="h-7 m-4 text-black-50" />
-        <SlBubble href="#" className="h-7 m-4 text-black-50" />
-        <SlUser href="#" className="h-7 m-4 text-black-50" />
-        <SlArrowDown href="#" className="h-7 m-4 text-black-50" />
+        <SlMagnifier href="#" className="h-7 m-4 text-black-50 cursor-pointer" />
+        <SlBell href="#" className="h-7 m-4 text-black-50 cursor-pointer" />
+        <SlBubble href="#" className="h-7 m-4 text-black-50 cursor-pointer" />
+        <SlUser href="#" className="h-7 m-4 text-black-50 cursor-pointer" />
+        <SlArrowDown href="#" className="h-7 m-4 text-black-50 cursor-pointer" />
       </div>
     </header>
   );
