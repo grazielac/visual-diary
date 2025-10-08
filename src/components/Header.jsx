423515ec32756d8e19197555a1f3d349a98a07b6
grazielac <cpgraziela@gmail.com>
2025-10-08 19:02:08 +0100

    Add search icon + chatbox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import Vd from "../assets/vd.png";
+import Search from "../assets/search.png";
+import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className="bg-amber-400 flex items-center">
-      <img src={Vd} alt="Site Logo" className="h-10 m-4" />
+
+      {/* left: logo + home feed */}
+      <div className="flex justify-between items-center">
+        <img src={Vd} alt="Site Logo" className="h-10 m-4" />
+        <a href="#" className="hover:underline mr-4 text-sm">Home Feed</a>
+      </div>
+      {/* right: profile + search + etc */}
+      <div>
+        <img src={Search} alt="Search Icon" className="h-7 m-4" />
+        <img src="https://www.flaticon.com/free-icons/chat-box" alt="Search Icon" className="h-7 m-4" />
+      </div>
     </header>
   );
 }
