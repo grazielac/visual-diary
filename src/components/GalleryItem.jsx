7ab79c269d3be6446a277a37b7eb40b88eb486da
grazielac <cpgraziela@gmail.com>
2025-10-09 15:53:23 +0100

    Fix: show dialog w/ 'fixed' position

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -2,27 +2,21 @@ import Gallery from "./Gallery";
 import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { useState } from "react";
 
-export default function GalleryItem({ close }) {
+export default function GalleryItem() {
   let [isOpen, setIsOpen] = useState(true);
-
-  function open() {
-    setIsOpen(true);
-  }
 
   return (
     <>
-      <Button
-        onClick={open}
-        className="rounded-md bg-black/20 px-4 py-2 font-medium text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-black/30"
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
       >
-        Open dialog
-      </Button>
-
-      <Dialog open={isOpen} onClose={close} className="relative z-50">
-        <div className="bg-amber-200">
+        <div className="fixed inset-1/15 p-4 bg-amber-200 border-1 rounded-xl">
           <DialogPanel className="bg-amber-300 p-12">
             <DialogTitle className="text-lg"></DialogTitle>
             <p>HELLO WORLD</p>
+            
             <div>
               <button onClick={() => setIsOpen(false)}>Close</button>
             </div>
